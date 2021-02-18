5cbe3443adc8107e3c8b703bb46607be74aaa7cb
Kert Prink <prinkkert@gmail.com>
2021-02-18 21:33:08 +0200

    PQD-FRONT-2: jira sprint info

--- src/views/products/Dashboard.js
+++ src/views/products/Dashboard.js
@@ -1,6 +1,6 @@
 import {
     CAlert, CBadge, CCard, CCardBody, CCardHeader, CCol, CDropdown, CDropdownItem, CDropdownMenu, CDropdownToggle,
-    CListGroup, CListGroupItem, CRow, CTabContent, CTabPane
+    CLink, CListGroup, CListGroupItem, CRow, CTabContent, CTabPane, CTooltip
 } from "@coreui/react";
 import React, {useEffect, useRef, useState} from "react";
 import {CChartLine} from "@coreui/react-chartjs";
@@ -130,14 +130,23 @@ export const Dashboard = (props) => {
             const qualityLevels = selectedProduct.releaseInfo.map(rel => rel.qualityLevel * 100);
             const releaseIds = selectedProduct.releaseInfo.map(rel => {
                 const timestamp = rel.created;
-                const date = new Date(timestamp);
-                return date.getDate() + ". " + date.toString().split(" ")[1] + " " + date.getFullYear() +
-                    ", " + date.getHours() + ":" + date.getMinutes();
+                return getTimeFromTimestamp(timestamp);
             });
             setQualityLevels(qualityLevels);
             setReleaseIds(releaseIds);
         }
     }, [selectedProduct])
+
+    const getTimeFromTimestamp = (timestamp) => {
+        const date = new Date(timestamp);
+        return date.getDate() + ". " + date.toString().split(" ")[1] + " " + date.getFullYear() +
+            ", " + date.getHours() + ":" + date.getMinutes();
+    }
+
+    const getTimeFromTimestampWithoutClock = (timestamp) => {
+        const date = new Date(timestamp);
+        return date.getDate() + ". " + date.toString().split(" ")[1] + " " + date.getFullYear();
+    }
 
     const onDropdownItemClick = (idx, rel) => {
         const selRel = selectedProduct.releaseInfo[idx]
@@ -237,13 +246,13 @@ export const Dashboard = (props) => {
                                       ? <CListGroupItem onClick={() => setActiveTab(1)} action
                                                         active={activeTab === 1}>
                                           <CIcon name="cil-rss"/> Sonarqube
-                                     </CListGroupItem>
+                                      </CListGroupItem>
                                       : null}
                                      {selectedRelease.releaseInfoJira
                                       ? <CListGroupItem onClick={() => setActiveTab(2)} action
                                                         active={activeTab === 2}>
                                           <CIcon name="cib-jira"/> Jira
-                                     </CListGroupItem>
+                                      </CListGroupItem>
                                       : null}
                                  </CListGroup>
                              </CCol>
@@ -253,7 +262,7 @@ export const Dashboard = (props) => {
                                          <CCard>
                                              <CCardHeader>
                                                  <CIcon name="cil-rss"/> Sonarqube
-                                                 <small> scan info</small>
+                                                 <small> master scan info</small>
                                              </CCardHeader>
                                              <CCardBody>
                                                  <CListGroup>
@@ -312,27 +321,57 @@ export const Dashboard = (props) => {
                                              </CCardBody>
                                          </CCard>
                                      </CTabPane>
+
                                      <CTabPane active={activeTab === 2}>
-                                         <p>Ut ut do pariatur aliquip aliqua aliquip exercitation do nostrud commodo
-                                            reprehenderit aute ipsum voluptate. Irure Lorem et laboris
-                                            nostrud amet cupidatat cupidatat anim do ut velit mollit consequat enim
-                                            tempor. Consectetur
-                                            est minim nostrud nostrud consectetur irure labore voluptate irure. Ipsum id
-                                            Lorem sit sint voluptate est pariatur eu ad cupidatat et
-                                            deserunt culpa sit eiusmod deserunt. Consectetur et fugiat anim do eiusmod
-                                            aliquip nulla
-                                            laborum elit adipisicing pariatur cillum.</p>
-                                     </CTabPane>
-                                     <CTabPane active={activeTab === 3}>
-                                         <p>Irure enim occaecat labore sit qui aliquip reprehenderit amet velit.
-                                            Deserunt ullamco ex elit nostrud ut dolore nisi officia magna
-                                            sit occaecat laboris sunt dolor. Nisi eu minim cillum occaecat aute est
-                                            cupidatat aliqua labore
-                                            aute occaecat ea aliquip sunt amet. Aute mollit dolor ut exercitation irure
-                                            commodo non amet consectetur quis amet culpa. Quis ullamco
-                                            nisi amet qui aute irure eu. Magna labore dolor quis ex labore id nostrud
-                                            deserunt dolor
-                                            eiusmod eu pariatur culpa mollit in irure.</p>
+                                         <CCard>
+                                             <CCardHeader>
+                                                 <CIcon name="cib-jira"/> Atlassian Jira
+                                                 <small> scrum sprint info</small>
+                                             </CCardHeader>
+                                             <CCardBody>
+                                                 <CListGroup>
+                                                     <CListGroupItem color="light">
+                                                         Active Sprint(s) <small>at the time of the release</small>
+                                                     </CListGroupItem>
+
+                                                     {selectedRelease.releaseInfoJira.jiraSprints.map(sprint => {
+                                                         return <CListGroupItem>
+                                                             <CRow>
+                                                                 <CCol xs="4">
+                                                                     {sprint.goal
+                                                                      ? <CTooltip content={sprint.goal}>
+                                                                          <CLink>
+                                                                              <div style={{fontSize: "medium"}}>
+                                                                                  {sprint.name}
+                                                                              </div>
+                                                                          </CLink>
+                                                                      </CTooltip>
+                                                                      : <div style={{fontSize: "medium"}}>
+                                                                          {sprint.name}
+                                                                      </div>}
+                                                                 </CCol>
+                                                                 <CCol xs="5">
+                                                                     <small>
+                                                                         <CIcon name="cil-clock"/>{" "}
+                                                                         {getTimeFromTimestampWithoutClock(
+                                                                             sprint.start)} - {getTimeFromTimestampWithoutClock(
+                                                                         sprint.end)}
+                                                                     </small>
+                                                                 </CCol>
+                                                                 <CCol xs="3">
+                                                                     <a target="_blank"
+                                                                        href={sprint.browserUrl}>
+                                                                         Open Jira
+                                                                         <CIcon name="cil-external-link"/>
+                                                                     </a>
+                                                                 </CCol>
+                                                             </CRow>
+                                                         </CListGroupItem>
+                                                     })}
+
+                                                 </CListGroup>
+                                             </CCardBody>
+                                         </CCard>
                                      </CTabPane>
                                  </CTabContent>
                              </CCol>
@@ -343,43 +382,41 @@ export const Dashboard = (props) => {
             );
         }
 
-    return (
-        <CCardBody>
-            <CAlert color="info">
-                No release info to display. Trigger release info collection on this product to see initial
-                results
-            </CAlert>
-        </CCardBody>
-    );
+        return (
+            <CCardBody>
+                <CAlert color="info">
+                    No release info to display. Trigger release info collection on this product to see initial
+                    results
+                </CAlert>
+            </CCardBody>
+        );
     }
 
-    const renderRedirect = () =>
-        {
-            return (
-                <Switch>
-                    <Redirect to="/products/all"/>
-                </Switch>
-            )
-        }
+    const renderRedirect = () => {
+        return (
+            <Switch>
+                <Redirect to="/products/all"/>
+            </Switch>
+        )
+    }
 
-    const renderBody = () =>
-        {
-            switch (componentState) {
-                case ComponentStates.Loading:
-                    return renderLoader();
-                case ComponentStates.Displaying:
-                    return renderDetailView();
-                case ComponentStates.Redirect: // fall-through
-                default:
-                    return renderRedirect();
-            }
+    const renderBody = () => {
+        switch (componentState) {
+            case ComponentStates.Loading:
+                return renderLoader();
+            case ComponentStates.Displaying:
+                return renderDetailView();
+            case ComponentStates.Redirect: // fall-through
+            default:
+                return renderRedirect();
         }
+    }
 
     return (
         <CCard>
             {renderBody()}
         </CCard>
     );
-    }
+}
 
-    export default Dashboard
+export default Dashboard
